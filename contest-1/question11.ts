93f6e82f7010f4ab521ccb1c640552c4c44a5f63
atoms19 <atomicwave19@gmail.com>
2026-06-09 17:17:11 +0530

    initial commit

--- contest-1/question11.ts
+++ contest-1/question11.ts
@@ -1,4 +1,24 @@
 let obj = { fruits: ["apple","apple","banana"], drinks: ["apple","tea"] }
 
 
+let mao = new Map<String,number>()
+for(const [key,values] of Object.entries(obj)){
+    for(let val of values){
+		 if(mao.get(val)==undefined) {
+			mao.set(val,0)
+		  }
+		  mao.set(val,mao.get(val)+1) 
+	 }
+}
+let max:[String,number]|undefined=undefined;
+for(const entry of mao){
+   if(!max || entry[1] > max[1]){
+	 max = entry 
+  }
+}
+
+console.log(max[0])
+
+
+
 
